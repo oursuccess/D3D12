112bc441a07cbfb304fa514a9e6a63fc507328fd
Je <xanderzhang@lilith.com>
2022-03-14 10:06:51 +0800

    一道第二题直接搞了一个类 还没搞完

---
+++ QuizCommonHeader.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "d3d12book-master/Common/d3dApp.h"
+#include "d3d12book-master/Common/MathHelper.h"
+#include "d3d12book-master/Common/UploadBuffer.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
