b2ae0b5bf617073817e8c13e3ba42190ad73038f
Je <xanderzhang@lilith.com>
2022-03-21 11:16:26 +0800

    抄了一下draw

--- ch07/Ch07Shapes.cpp
+++ ch07/Ch07Shapes.cpp
@@ -25,13 +25,13 @@ struct RenderItem
 	int BaseVertexLocation = 0;
 };
 
-class ShapesApp : public D3DApp
+class JeCh07ShapesApp : public D3DApp
 {
 public:
-	ShapesApp(HINSTANCE hInstance);
-	ShapesApp(const ShapesApp& rhs) = delete;
-	ShapesApp& operator=(const ShapesApp& rhs) = delete;
-	~ShapesApp();
+	JeCh07ShapesApp(HINSTANCE hInstance);
+	JeCh07ShapesApp(const JeCh07ShapesApp& rhs) = delete;
+	JeCh07ShapesApp& operator=(const JeCh07ShapesApp& rhs) = delete;
+	~JeCh07ShapesApp();
 
 	virtual bool Initialize() override;
 
@@ -56,10 +56,11 @@ private:
 	void BuildShapeGeometry();
 	void BuildPSOs();
 	void BuildFrameResources();
+	void BuildRenderItems();
 	void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems);
 
 private:
-	std::vector<std::unique_ptr<FrameResource>> gNumFrameResources;
+	std::vector<std::unique_ptr<FrameResource>> mFrameResources;
 	FrameResource* mCurrFrameResource = nullptr;
 	int mCurrFrameResourceIndex = 0;
 
@@ -79,6 +80,8 @@ private:
 
 	PassConstants mMainPassCB;
 
+	UINT mPassCbvOffset = 0;
+
 	bool mIsWireframe = false;
 
 	XMFLOAT3 mEyePos = { 0.0f, 0.0f, 0.0f };
@@ -91,3 +94,214 @@ private:
 
 	POINT mLastMousePos;
 };
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int ShowCmd)
+{
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		JeCh07ShapesApp theApp(hInstance);
+		if (!theApp.Initialize()) return 0;
+
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
+
+JeCh07ShapesApp::JeCh07ShapesApp(HINSTANCE hInstance) : D3DApp(hInstance)
+{
+}
+
+JeCh07ShapesApp::~JeCh07ShapesApp()
+{
+	if (md3dDevice != nullptr) FlushCommandQueue();
+}
+
+bool JeCh07ShapesApp::Initialize()
+{
+	if (!D3DApp::Initialize()) return false;
+
+	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+	BuildRootSignature();
+	BuildShadersAndInputLayout();
+	BuildShapeGeometry();
+	BuildRenderItems();
+	BuildFrameResources();
+	BuildDesciptorHeaps();
+	BuildConstantBufferViews();
+	BuildPSOs();
+
+	//execute the initialization commands
+	ThrowIfFailed(mCommandList->Close());
+	ID3D12CommandList* cmdLists[] = { mCommandList.Get() };
+	mCommandQueue->ExecuteCommandLists(_countof(cmdLists), cmdLists);
+
+	//wait until initialization is complete
+	FlushCommandQueue();
+
+	return true;
+}
+
+void JeCh07ShapesApp::OnResize()
+{
+	D3DApp::OnResize();
+
+	//update the aspect ratio and recompute the projection matrix
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f * MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	XMStoreFloat4x4(&mProj, P);
+}
+
+void JeCh07ShapesApp::Update(const GameTimer& gt)
+{
+	OnKeyboardInput(gt);
+	UpdateCamera(gt);
+
+	//cycle through the circular frame resource array
+	mCurrFrameResourceIndex = (mCurrFrameResourceIndex + 1) % gNumFrameResources;
+	mCurrFrameResource = mFrameResources[mCurrFrameResourceIndex].get();
+
+	//Has the GPU finished processing the commands of the current frame resource?
+	//If not, wait until the GPU has completed commands up to this fence point
+	if (mCurrFrameResource->Fence != 0 && mFence->GetCompletedValue() <= mCurrFrameResource->Fence)
+	{
+		HANDLE eventHandle = CreateEventEx(nullptr, false, false, EVENT_ALL_ACCESS);
+		ThrowIfFailed(mFence->SetEventOnCompletion(mCurrFrameResource->Fence, eventHandle));
+		WaitForSingleObject(eventHandle, INFINITE);
+		CloseHandle(eventHandle);
+	}
+
+	UpdateObjectCBs(gt);
+	UpdateMainPassCB(gt);
+}
+
+void JeCh07ShapesApp::Draw(const GameTimer& gt)
+{
+	auto cmdListAlloc = mCurrFrameResource->CmdListAlloc;
+	ThrowIfFailed(cmdListAlloc->Reset());
+
+	if (mIsWireframe) 
+	{
+		ThrowIfFailed(mCommandList->Reset(cmdListAlloc.Get(), mPSOs["opaque_wireframe"].Get()));
+	}
+	else
+	{
+		ThrowIfFailed(mCommandList->Reset(cmdListAlloc.Get(), mPSOs["opaque"].Get()));
+	}
+
+	mCommandList->RSSetViewports(1, &mScreenViewport);
+	mCommandList->RSSetScissorRects(1, &mScissorRect);
+
+	//indicate a state transition on the resource usage
+	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
+		D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
+
+	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
+	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
+
+	//specify the back buffer and depth buffer
+	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
+
+	ID3D12DescriptorHeap* descriptorHeaps[] = { mCbvHeap.Get() };
+	mCommandList->SetDescriptorHeaps(_countof(descriptorHeaps), descriptorHeaps);
+
+	mCommandList->SetGraphicsRootSignature(mRootSignature.Get());
+
+	int passCbvIndex = mPassCbvOffset + mCurrFrameResourceIndex;
+	auto passCbvHandle = CD3DX12_GPU_DESCRIPTOR_HANDLE(mCbvHeap->GetGPUDescriptorHandleForHeapStart());
+	passCbvHandle.Offset(passCbvIndex, mCbvSrvUavDescriptorSize);
+	mCommandList->SetGraphicsRootDescriptorTable(1, passCbvHandle);
+
+	DrawRenderItems(mCommandList.Get(), mOpaqueRitems);
+
+	//indicate a state transition on the resource usage
+	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
+		D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+	//done recording commands
+	ThrowIfFailed(mCommandList->Close());
+
+	//add the command list to the queue for execution
+	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+	//swap the back and front buffers
+	ThrowIfFailed(mSwapChain->Present(0, 0));
+	mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
+
+	//advance the fence value to mark commands up to this fence point
+	mCurrFrameResource->Fence = ++mCurrentFence;
+
+	mCommandQueue->Signal(mFence.Get(), mCurrentFence);
+}
+
+void JeCh07ShapesApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+}
+
+void JeCh07ShapesApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+}
+
+void JeCh07ShapesApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+}
+
+void JeCh07ShapesApp::OnKeyboardInput(const GameTimer& gt)
+{
+}
+
+void JeCh07ShapesApp::UpdateCamera(const GameTimer& gt)
+{
+}
+
+void JeCh07ShapesApp::UpdateObjectCBs(const GameTimer& gt)
+{
+}
+
+void JeCh07ShapesApp::UpdateMainPassCB(const GameTimer& gt)
+{
+}
+
+void JeCh07ShapesApp::BuildDesciptorHeaps()
+{
+}
+
+void JeCh07ShapesApp::BuildConstantBufferViews()
+{
+}
+
+void JeCh07ShapesApp::BuildRootSignature()
+{
+}
+
+void JeCh07ShapesApp::BuildShadersAndInputLayout()
+{
+}
+
+void JeCh07ShapesApp::BuildRenderItems()
+{
+}
+
+void JeCh07ShapesApp::BuildShapeGeometry()
+{
+}
+
+void JeCh07ShapesApp::BuildPSOs()
+{
+}
+
+void JeCh07ShapesApp::BuildFrameResources()
+{
+}
+
+void JeCh07ShapesApp::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems)
+{
+}
