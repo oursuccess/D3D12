a7a5e258cabc9368761a2c37d3e5da8f3f88038a
Je <xanderzhang@lilith.com>
2022-03-21 09:02:08 +0800

    抄了一下FrameResource(帧资源)

---
+++ ch07/Ch07Shapes.cpp
@@ -0,0 +1,1 @@
+#include "../QuizCommonHeader.h"
---
+++ ch07/FrameResource.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "../d3d12book-master/Common/d3dUtil.h"
+#include "../d3d12book-master/Common/MathHelper.h"
+#include "../d3d12book-master/Common/UploadBuffer.h"
+
+struct ObjectConstants
+{
+	DirectX::XMFLOAT4X4 World = MathHelper::Identity4x4();
+};
+
+struct PassConstants
+{
+	DirectX::XMFLOAT4X4 View = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 InvView = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 Proj = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 InvProj = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 ViewProj = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 InvViewProj = MathHelper::Identity4x4();
+	DirectX::XMFLOAT3 EyePosW = { 0.0f, 0.0f, 0.0f };
+	//why??
+	float cbPerObjectPad1 = 0.0f;
+	DirectX::XMFLOAT2 RenderTargetSize = { 0.0f, 0.0f };
+	DirectX::XMFLOAT2 InvRenderTargetSize = { 0.0f, 0.0f };
+	float NearZ = 0.0f;
+	float FarZ = 0.0f;
+	float TotalTime = 0.0f;
+	float DeltaTime = 0.0f;
+};
+
+struct VerTex 
+{
+	DirectX::XMFLOAT3 Pos;
+	DirectX::XMFLOAT4 Color;
+};
+
+//stores the resource needed for the CPU to build the command lists for a frame
+class FrameResource
+{
+public:
+	FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount);
+	FrameResource(const FrameResource& rhs) = delete;
+	FrameResource& operator=(const FrameResource& rhs) = delete;
+	~FrameResource();
+
+	//each frame needs their own allocator
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> CmdListAlloc;
+
+	//each frame need their own cbuffers
+	std::unique_ptr<UploadBuffer<PassConstants>> PassCB = nullptr;
+	std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectCB = nullptr;
+
+	//Fence
+	UINT64 Fence = 0;
+};
+
