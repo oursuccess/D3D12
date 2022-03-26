b3fec2a8ff604af3e66109ce155342d72553b516
Je <xanderzhang@lilith.com>
2022-03-27 00:02:58 +0800

    抄完了LitWaves 但是没测试 没写shader

--- QuizCommonHeader.h
+++ QuizCommonHeader.h
@@ -3,6 +3,7 @@
 #include "d3d12book-master/Common/d3dApp.h"
 #include "d3d12book-master/Common/MathHelper.h"
 #include "d3d12book-master/Common/UploadBuffer.h"
+#include "d3d12book-master/Common/GeometryGenerator.h"
 
 using Microsoft::WRL::ComPtr;
 using namespace DirectX;
