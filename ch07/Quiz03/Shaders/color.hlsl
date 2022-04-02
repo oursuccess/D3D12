e37363e5b3421185816cf5ad2a75bc1977b90a27
Asakawa <xanderzhang@lilith.com>
2022-04-02 08:35:34 +0800

    准备添加第3题注释

---
+++ ch07/Quiz03/Shaders/color.hlsl
@@ -0,0 +1,50 @@
+//copy of color.hlsl of Frank Luna. see ch06
+
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorldViewProj;
+};
+
+cbuffer cbPass : register(b1)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gTotalTime;
+    float gDeltaTime;
+};
+
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float4 Color : COLOR;
+};
+
+struct VertexOut
+{
+    float4 PosH : SV_POSITION;
+    float4 Color : COLOR;
+};
+
+VertexOut VS(VertexIn vin)
+{
+    VertexOut vout;
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
+    vout.PosH = mul(posW, gViewProj);
+    vout.Color = vin.Color;
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    return pin.Color;
+}
