c63f84adf9588c357484a458b1115dc00a71f0b4
Asakawa <xanderzhang@lilith.com>
2022-04-05 09:00:02 +0800

    准备添加第2题的答案

---
+++ ch08/Quiz02/Shaders/Default.hlsl
@@ -0,0 +1,99 @@
+//copy of Default.hlsl by Frank Luna, ch08
+
+// Defaults for number of lights.
+#ifndef NUM_DIR_LIGHTS
+    #define NUM_DIR_LIGHTS 1
+#endif
+
+#ifndef NUM_POINT_LIGHTS
+    #define NUM_POINT_LIGHTS 0
+#endif
+
+#ifndef NUM_SPOT_LIGHTS
+    #define NUM_SPOT_LIGHTS 0
+#endif
+
+#include "LightingUtil.hlsl"
+
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorld;
+};
+
+cbuffer cbMaterial : register(b1)
+{
+    float4 gDiffuseAlbedo;
+    float3 gFresnelR0;
+    float gRoughness;
+    float4x4 gMatTransform;
+};
+
+cbuffer cbPass : register(b2)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gTotalTime;
+    float gDeltaTime;
+
+    float4 gAmbientLight;
+    Light gLights[MaxLights];
+};
+
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+};
+
+struct VertexOut
+{
+    float4 PosH : SV_POSITION;
+    float3 PosW : POSITION;
+    float3 NormalW : NORMAL;
+};
+
+VertexOut VS(VertexIn vin)
+{
+    VertexOut vout = (VertexOut) 0.0f;
+    
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosW = posW.xyz;
+    
+    vout.NormalW = mul(vin.NormalL, (float3x3) gWorld);
+    
+    vout.PosH = mul(posW, gViewProj);
+    
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    pin.NormalW = normalize(pin.NormalW);
+
+    float3 toEyeW = normalize(gEyePosW - pin.PosW);
+    float4 ambient = gAmbientLight * gDiffuseAlbedo;
+    
+    //原来从粗糙度到光泽度的转换在这里。。。
+    const float shininess = 1.0f - gRoughness;
+
+    Material mat = { gDiffuseAlbedo, gFresnelR0, shininess };
+    float3 shadowFactor = 1.0f;
+    
+    float4 directLight = ComputeLighting(gLights, mat, pin.PosW, pin.NormalW, toEyeW, shadowFactor);
+
+    float4 litColor = ambient + directLight;
+
+    litColor.a = gDiffuseAlbedo.a;
+
+    return litColor;
+}
