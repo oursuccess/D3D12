de6ef562799eb3e25cfcb4fc6c7e0594931c209e
Je <xanderzhang@lilith.com>
2022-05-10 10:38:39 +0800

    传入了没有细分的20面体

---
+++ ch14/Quiz02/Shaders/Tessellation.hlsl
@@ -0,0 +1,130 @@
+//copy of BezierTessellation by Frank Luna, ch14
+
+#include "LightingUtil.hlsl"
+
+Texture2D gDiffuseMap : register(t0);
+
+SamplerState gSamPoint : register(s0);
+
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorld;
+    float4x4 gTexTransform;
+};
+
+cbuffer cbPass : register(b1)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gTotalTime;
+    float gDeltaTime;
+    float4 gAmbientLight;
+    
+    float4 gFogColor;
+    float gFogStart;
+    float gFogRange;
+    float2 cbPerObjectPad2;
+
+    Light gLights[MaxLights];
+};
+
+cbuffer cbMaterial : register(b2)
+{
+    float4 gDiffuseAlbedo;
+    float3 gFresnelR0;
+    float gRoughness;
+    float4x4 gMatTransform;
+};
+
+struct VertexIn
+{
+    float3 PosL : POSITION;
+};
+
+struct VertexOut
+{
+    float3 PosL : POSITION;
+};
+
+VertexOut VS(VertexIn vin)
+{
+    VertexOut vout;
+
+    vout.PosL = vin.PosL;
+
+    return vout;
+}
+
+struct PatchTess
+{
+    float EdgeTess[3] : SV_TessFactor;
+    float InsideTess[1] : SV_InsideTessFactor;
+};
+
+PatchTess ConstantHS(InputPatch<VertexOut, 3> patch, uint patchID : SV_PrimitiveID)
+{
+    PatchTess pt;
+
+    pt.EdgeTess[0] = 1;
+    pt.EdgeTess[1] = 1;
+    pt.EdgeTess[2] = 1;
+
+    pt.InsideTess[0] = 1;
+    return pt;
+}
+
+struct HullOut
+{
+    float3 PosL : POSITION;
+};
+
+[domain("tri")]
+[partitioning("integer")]
+[outputtopology("triangle_cw")]
+[outputcontrolpoints(3)]
+[patchconstantfunc("ConstantHS")]
+[maxtessfactor(64.0f)]
+HullOut HS(InputPatch<VertexOut, 3> p, uint i : SV_OutputControlPointID,
+    uint patchId : SV_PrimitiveID)
+{
+    HullOut hout;
+    hout.PosL = p[i].PosL;
+    return hout;
+}
+
+struct DomainOut
+{
+    float4 PosH : SV_Position;
+};
+
+[domain("tri")]
+DomainOut DS(PatchTess patchTess, float3 uvw : SV_DomainLocation,
+    const OutputPatch<HullOut, 3> bezPatch)
+{
+    DomainOut dout;
+
+    //float3 patchNormal = normalize(bezPatch[0].PosL + bezPatch[1].PosL + bezPatch[2].PosL);
+    float3 p = normalize(bezPatch[0].PosL * uvw.x + bezPatch[1].PosL * uvw.y + bezPatch[2].PosL * uvw.z);
+    //float r = 20.0f;
+    //p *= r;
+
+    float4 posW = mul(float4(p, 1.0f), gWorld);
+    dout.PosH = mul(posW, gViewProj);
+
+    return dout;
+}
+
+float4 PS(DomainOut pin) : SV_Target
+{
+    return float4(1.0f, 1.0f, 1.0f, 1.0f);
+}
