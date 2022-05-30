04b95fd5a44611ec063dd379ff82673e617f9fc2
Je <xanderzhang@lilith.com>
2022-05-30 09:10:23 +0800

    添加第3题答案

---
+++ ch18/Quiz03/Shaders/Sky.hlsl
@@ -0,0 +1,44 @@
+//copy of Sky.hlsl by Frank Luna, ch18
+//天空纹理简单返回了采样立方体图的数据。 用来绘制天空
+//为了防止穿帮，并模拟远处的天空，因此我们总是将采样点放在相机上，并总是让PosH(投影矩阵)的z等于w
+
+// Include common HLSL code.
+#include "Common.hlsl"
+
+struct VertexIn
+{
+	float3 PosL    : POSITION;
+	float3 NormalL : NORMAL;
+	float2 TexC    : TEXCOORD;
+};
+
+struct VertexOut
+{
+	float4 PosH : SV_POSITION;
+    float3 PosL : POSITION;
+};
+ 
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout;
+
+	// Use local vertex position as cubemap lookup vector.
+	vout.PosL = vin.PosL;
+	
+	// Transform to world space.
+	float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+
+	// Always center sky about camera.
+	posW.xyz += gEyePosW;
+
+	// Set z = w so that z/w = 1 (i.e., skydome always on far plane).
+	vout.PosH = mul(posW, gViewProj).xyww;
+	
+	return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    return gCubeMap.Sample(gsamLinearWrap, pin.PosL);
+}
+
