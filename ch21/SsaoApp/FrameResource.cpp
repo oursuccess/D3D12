29b3e9c4adcc19417b2556448a53ca2b10f0c5f4
Je <xanderzhang@lilith.com>
2022-06-27 09:07:37 +0800

    抄了一下FrameResource

--- ch21/SsaoApp/FrameResource.cpp
+++ ch21/SsaoApp/FrameResource.cpp
@@ -1,1 +1,16 @@
 #include "FrameResource.h"
+
+FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount)
+{
+	ThrowIfFailed(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, 
+		IID_PPV_ARGS(CmdListAlloc.GetAddressOf())));
+
+	PassCB = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, true);
+	SsaoCB = std::make_unique<UploadBuffer<SsaoConstants>>(device, 1, true);	//环境光遮蔽需要的常量只需要1份
+	MaterialBuffer = std::make_unique<UploadBuffer<MaterialData>>(device, materialCount, true);
+	ObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, passCount, true);
+}
+
+FrameResource::~FrameResource()
+{
+}
