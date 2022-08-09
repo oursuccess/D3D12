c2a7bc1ac964129bd75df47f2d6761bb94d1cc08
Je <xanderzhang@lilith.com>
2022-08-09 10:53:06 +0800

    抄了一下SkinnedData.h

--- ch23/SkinnedMesh/SkinnedData.cpp
+++ ch23/SkinnedMesh/SkinnedData.cpp
@@ -1,1 +1,60 @@
 #include "SkinnedData.h"
+
+Keyframe::Keyframe()
+{
+}
+
+Keyframe::~Keyframe()
+{
+}
+
+float BoneAnimation::GetStartTime() const
+{
+	return 0.0f;
+}
+
+float BoneAnimation::GetEndTime() const
+{
+	return 0.0f;
+}
+
+void BoneAnimation::Interpolate(float t, DirectX::XMFLOAT4X4& M) const
+{
+}
+
+float AnimationClip::GetClipStartTime() const
+{
+	return 0.0f;
+}
+
+float AnimationClip::GetClipEndTime() const
+{
+	return 0.0f;
+}
+
+void AnimationClip::Interpolate(float t, std::vector<DirectX::XMFLOAT4X4>& boneTransforms) const
+{
+}
+
+UINT SkinnedData::BoneCount() const
+{
+	return 0;
+}
+
+float SkinnedData::GetClipStartTime(const std::string& clipName) const
+{
+	return 0.0f;
+}
+
+float SkinnedData::GetClipEndTime(const std::string& clipName) const
+{
+	return 0.0f;
+}
+
+void SkinnedData::Set(std::vector<int>& boneHierarchy, std::vector<DirectX::XMFLOAT4X4>& boneOffsets, std::unordered_map<std::string, AnimationClip>& animations)
+{
+}
+
+void SkinnedData::GetFinalTransforms(const std::string& clipName, float timePos, std::vector<DirectX::XMFLOAT4X4>& finalTransforms) const
+{
+}
