54bc5029d18daaf3b28e896967c745ae51fa897b
Je <xanderzhang@lilith.com>
2022-08-13 17:47:37 +0800

    添加类声明中每个变量的注释

--- ch23/SkinnedMesh/SkinnedMeshApp.cpp
+++ ch23/SkinnedMesh/SkinnedMeshApp.cpp
@@ -119,74 +119,74 @@ private:
 	std::array<const CD3DX12_STATIC_SAMPLER_DESC, 7> GetStaticSamplers();	//获取静态采样器们. 我们在这里定义了7个静态采样器
 
 private:
-	std::vector<std::unique_ptr<FrameResource>> mFrameResources;
-	FrameResource* mCurrFrameResource = nullptr;
-	int mCurrFrameResourceIndex = 0;
+	std::vector<std::unique_ptr<FrameResource>> mFrameResources;	//存储帧资源们
+	FrameResource* mCurrFrameResource = nullptr;	//当前使用的帧资源
+	int mCurrFrameResourceIndex = 0;	//记录当前使用的帧资源的下标
 
-	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
-	ComPtr<ID3D12RootSignature> mSsaoRootSignature = nullptr;
+	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;	//主Pass的根签名
+	ComPtr<ID3D12RootSignature> mSsaoRootSignature = nullptr;	//SsaoPass的根签名
 
-	ComPtr<ID3D12DescriptorHeap> mSrvDescriptorHeap = nullptr;
+	ComPtr<ID3D12DescriptorHeap> mSrvDescriptorHeap = nullptr;	//srv的描述符堆
 
-	std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> mGeometries;
+	std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> mGeometries;	//分别记录了几何，材质，纹理，Shader和PSO
 	std::unordered_map<std::string, std::unique_ptr<Material>> mMaterials;
 	std::unordered_map<std::string, std::unique_ptr<Texture>> mTextures;
 	std::unordered_map<std::string, ComPtr<ID3DBlob>> mShaders;
 	std::unordered_map<std::string, ComPtr<ID3D12PipelineState>> mPSOs;
 
-	std::vector<D3D12_INPUT_ELEMENT_DESC> mInputLayout;
-	std::vector<D3D12_INPUT_ELEMENT_DESC> mSkinnedInputLayout;
+	std::vector<D3D12_INPUT_ELEMENT_DESC> mInputLayout;	//通常顶点的输入布局描述
+	std::vector<D3D12_INPUT_ELEMENT_DESC> mSkinnedInputLayout;	//动画的输入布局描述. 动画中添加了骨骼和权重, 因此我们需要向VS传入更多数据和语义
 
-	std::vector<std::unique_ptr<RenderItem>> mAllRitems;
-	std::vector<RenderItem*> mRitemLayer[(int)RenderLayer::Count];
+	std::vector<std::unique_ptr<RenderItem>> mAllRitems;	//记录所有的渲染项
+	std::vector<RenderItem*> mRitemLayer[(int)RenderLayer::Count];	//记录每个层级中的渲染项
 
-	UINT mSkyTexHeapIndex = 0;
-	UINT mShadowMapHeapIndex = 0;
-	UINT mSsaoHeapIndexStart = 0;
-	UINT mSsaoAmbientMapIndex = 0;
+	UINT mSkyTexHeapIndex = 0;	//记录了天空纹理在描述符堆中的索引
+	UINT mShadowMapHeapIndex = 0;	//记录了阴影图在描述符堆中的索引
+	UINT mSsaoHeapIndexStart = 0;	//记录了Ssao图在描述符堆中的索引的起始位置
+	UINT mSsaoAmbientMapIndex = 0;	//记录了Ssao遮蔽率图在描述符堆中的索引
 
-	UINT mNullCubeSrvIndex = 0;
+	UINT mNullCubeSrvIndex = 0;	//下面是几个我们不会真正使用，仅仅是为了绑定资源而创建的空Srv的索引， 分别是一个立方体图着色器资源，两个正常纹理着色器资源
 	UINT mNullTexSrvIndex1 = 0;
 	UINT mNullTexSrvIndex2 = 0;
 
-	CD3DX12_GPU_DESCRIPTOR_HANDLE mNullSrv;
+	CD3DX12_GPU_DESCRIPTOR_HANDLE mNullSrv;	//记录了我们需要将空Srv绑定到的GPU中的位置
 
-	PassConstants mMainPassCB;
-	PassConstants mShadowPassCB;
+	PassConstants mMainPassCB;	//主Pass对应的帧常量们
+	PassConstants mShadowPassCB;	//阴影Pass对应的帧常量们
 
-	UINT mSkinnedSrvHeapStart = 0;
-	std::string mSkinnedModelFilename = "Models\\soldier.m3d";
-	std::unique_ptr<SkinnedModelInstance> mSkinnedModelInst;
-	SkinnedData mSkinnedInfo;
-	std::vector<M3DLoader::Subset> mSkinnedSubsets;
-	std::vector<M3DLoader::M3dMaterial> mSkinnedMats;
-	std::vector<std::string> mSkinnedTextureNames;
+	UINT mSkinnedSrvHeapStart = 0;	//动画对应的着色器资源在描述符堆中的索引的起始位置
+	std::string mSkinnedModelFilename = "Models\\soldier.m3d";	//动画模型的相对路径与名称
+	std::unique_ptr<SkinnedModelInstance> mSkinnedModelInst;	//指向根据上面的动画模型而创建的蒙皮模型的实例的指针
+	SkinnedData mSkinnedInfo;	//记录了我们的动画模型对应的蒙皮、动画信息. 其包含了动画起始、结束时间、骨骼数量、模型空间到骨骼空间的变换矩阵、所有动画的信息
+	std::vector<M3DLoader::Subset> mSkinnedSubsets;	//记录了我们动画模型的submesh们
+	std::vector<M3DLoader::M3dMaterial> mSkinnedMats;	//记录了我们动画模型的每个submesh对应的材质
+	std::vector<std::string> mSkinnedTextureNames;	//我们在动画模型每个submesh中需要的纹理的名称
 
-	Camera mCamera;
+	Camera mCamera;	//本app使用的相机
 
-	std::unique_ptr<ShadowMap> mShadowMap;
-	std::unique_ptr<Ssao> mSsao;
+	std::unique_ptr<ShadowMap> mShadowMap;	//本app使用的阴影图。这里我们假定了阴影图只有一个
+	std::unique_ptr<Ssao> mSsao;	//本App使用的Ssao效果
 	
-	DirectX::BoundingSphere mSceneBounds;
+	DirectX::BoundingSphere mSceneBounds;	//包围了场景中所有物体的包围球
 
-	float mLightNearZ = 0.0f;
-	float mLightFarZ = 0.0f;
-	XMFLOAT3 mLightPosW;
-	XMFLOAT4X4 mLightView = MathHelper::Identity4x4();
-	XMFLOAT4X4 mLightProj = MathHelper::Identity4x4();
-	XMFLOAT4X4 mShadowTransform = MathHelper::Identity4x4();
+	float mLightNearZ = 0.0f;	//相机的NearZ
+	float mLightFarZ = 0.0f;	//相机的FarZ
+	XMFLOAT3 mLightPosW;	//光源的位置
+	XMFLOAT4X4 mLightView = MathHelper::Identity4x4();	//光源的观察矩阵. 用于将坐标从世界空间变换到相机的观察空间
+	XMFLOAT4X4 mLightProj = MathHelper::Identity4x4();	//光源的投影矩阵. 用于将坐标从相机的观察空间变换到NDC空间
+	XMFLOAT4X4 mShadowTransform = MathHelper::Identity4x4();	//阴影的变换矩阵. 用于直接将世界空间中的坐标变换到阴影采样纹理坐标
 
-	float mLightRotationAngle = 0.0f;
+	float mLightRotationAngle = 0.0f;	//光源当前的旋转角度
 
-	XMFLOAT3 mBaseLightDirections[3] = {
-		XMFLOAT3(0.57735f, -0.57735f, 0.57735f),
-		XMFLOAT3(-0.57735f, -0.57735f, 0.57735f),
-		XMFLOAT3(0.0f, -0.707f, -0.707f),
+	XMFLOAT3 mBaseLightDirections[3] = {	//我们使用了三点布光系统.
+		XMFLOAT3(0.57735f, -0.57735f, 0.57735f),	//左前方的光源向右下方照射
+		XMFLOAT3(-0.57735f, -0.57735f, 0.57735f),	//右前方的光源向左下方照射
+		XMFLOAT3(0.0f, -0.707f, -0.707f),	//上方的补光灯向前下方照射
 	};
 
-	XMFLOAT3 mRotatedLightDirections[3];
+	XMFLOAT3 mRotatedLightDirections[3];	//在计算过旋转角度后，每个光源当前的方向
 
-	POINT mLastMousePos;
+	POINT mLastMousePos;	//记录了鼠标上次的位置. 用于实现鼠标滑动的效果
 };
 
 SkinnedMeshApp::SkinnedMeshApp(HINSTANCE hInstance)
