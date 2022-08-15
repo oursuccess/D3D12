8c35dd0e64d94d543bec86677f29f63e53b2308a
Je <xanderzhang@lilith.com>
2022-08-15 15:26:51 +0800

    抄完了DrawRenderItems

--- ch23/SkinnedMesh/SkinnedMeshApp.cpp
+++ ch23/SkinnedMesh/SkinnedMeshApp.cpp
@@ -1380,16 +1380,196 @@ void SkinnedMeshApp::BuildMaterials()
 		mat->Roughness = mSkinnedMats[i].Roughness;
 		mat->MatCBIndex = matCBIndesx++;	//因为非蒙皮角色只用了4个材质
 
-		mMaterials[mat->Name] = mat;
+		mMaterials[mat->Name] = std::move(mat);
 	}
 }
 
 void SkinnedMeshApp::BuildRenderItems()
 {
+	auto skyRitem = std::make_unique<RenderItem>();	//我们开始构建每个渲染项
+	XMStoreFloat4x4(&skyRitem->World, XMMatrixScaling(5000.0f, 5000.0f, 5000.0f));	//天空盒尽量大即可
+	skyRitem->TexTransform = MathHelper::Identity4x4();
+	skyRitem->ObjCBIndex = 0;	//其对应的objCB为0
+	skyRitem->Mat = mMaterials["sky"].get();	//每个渲染项都有自己的材质、网格、拓扑、顶点、索引
+	skyRitem->Geo = mGeometries["shapeGeo"].get();
+	skyRitem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	skyRitem->IndexCount = skyRitem->Geo->DrawArgs["sphere"].IndexCount;
+	skyRitem->StartIndexLocation = skyRitem->Geo->DrawArgs["sphere"].StartIndexLocation;
+	skyRitem->BaseVertexLocation = skyRitem->Geo->DrawArgs["sphere"].BaseVertexLocation;
+
+	mRitemLayer[(int)RenderLayer::Sky].push_back(skyRitem.get());
+	mAllRitems.push_back(std::move(skyRitem));
+    
+    auto quadRitem = std::make_unique<RenderItem>();
+    quadRitem->World = MathHelper::Identity4x4();
+    quadRitem->TexTransform = MathHelper::Identity4x4();
+    quadRitem->ObjCBIndex = 1;
+    quadRitem->Mat = mMaterials["bricks0"].get();
+    quadRitem->Geo = mGeometries["shapeGeo"].get();
+    quadRitem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+    quadRitem->IndexCount = quadRitem->Geo->DrawArgs["quad"].IndexCount;
+    quadRitem->StartIndexLocation = quadRitem->Geo->DrawArgs["quad"].StartIndexLocation;
+    quadRitem->BaseVertexLocation = quadRitem->Geo->DrawArgs["quad"].BaseVertexLocation;
+
+    mRitemLayer[(int)RenderLayer::Debug].push_back(quadRitem.get());
+    mAllRitems.push_back(std::move(quadRitem));
+    
+	auto boxRitem = std::make_unique<RenderItem>();
+	XMStoreFloat4x4(&boxRitem->World, XMMatrixScaling(2.0f, 1.0f, 2.0f)*XMMatrixTranslation(0.0f, 0.5f, 0.0f));
+	XMStoreFloat4x4(&boxRitem->TexTransform, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	boxRitem->ObjCBIndex = 2;
+	boxRitem->Mat = mMaterials["bricks0"].get();
+	boxRitem->Geo = mGeometries["shapeGeo"].get();
+	boxRitem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	boxRitem->IndexCount = boxRitem->Geo->DrawArgs["box"].IndexCount;
+	boxRitem->StartIndexLocation = boxRitem->Geo->DrawArgs["box"].StartIndexLocation;
+	boxRitem->BaseVertexLocation = boxRitem->Geo->DrawArgs["box"].BaseVertexLocation;
+
+	mRitemLayer[(int)RenderLayer::Opaque].push_back(boxRitem.get());
+	mAllRitems.push_back(std::move(boxRitem));
+
+    auto gridRitem = std::make_unique<RenderItem>();
+    gridRitem->World = MathHelper::Identity4x4();
+	XMStoreFloat4x4(&gridRitem->TexTransform, XMMatrixScaling(8.0f, 8.0f, 1.0f));
+	gridRitem->ObjCBIndex = 3;
+	gridRitem->Mat = mMaterials["tile0"].get();
+	gridRitem->Geo = mGeometries["shapeGeo"].get();
+	gridRitem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+    gridRitem->IndexCount = gridRitem->Geo->DrawArgs["grid"].IndexCount;
+    gridRitem->StartIndexLocation = gridRitem->Geo->DrawArgs["grid"].StartIndexLocation;
+    gridRitem->BaseVertexLocation = gridRitem->Geo->DrawArgs["grid"].BaseVertexLocation;
+
+	mRitemLayer[(int)RenderLayer::Opaque].push_back(gridRitem.get());
+	mAllRitems.push_back(std::move(gridRitem));
+
+	XMMATRIX brickTexTransform = XMMatrixScaling(1.5f, 2.0f, 1.0f);
+	UINT objCBIndex = 4;
+	for(int i = 0; i < 5; ++i)
+	{
+		auto leftCylRitem = std::make_unique<RenderItem>();
+		auto rightCylRitem = std::make_unique<RenderItem>();
+		auto leftSphereRitem = std::make_unique<RenderItem>();
+		auto rightSphereRitem = std::make_unique<RenderItem>();
+
+		XMMATRIX leftCylWorld = XMMatrixTranslation(-5.0f, 1.5f, -10.0f + i*5.0f);
+		XMMATRIX rightCylWorld = XMMatrixTranslation(+5.0f, 1.5f, -10.0f + i*5.0f);
+
+		XMMATRIX leftSphereWorld = XMMatrixTranslation(-5.0f, 3.5f, -10.0f + i*5.0f);
+		XMMATRIX rightSphereWorld = XMMatrixTranslation(+5.0f, 3.5f, -10.0f + i*5.0f);
+
+		XMStoreFloat4x4(&leftCylRitem->World, rightCylWorld);
+		XMStoreFloat4x4(&leftCylRitem->TexTransform, brickTexTransform);
+		leftCylRitem->ObjCBIndex = objCBIndex++;
+		leftCylRitem->Mat = mMaterials["bricks0"].get();
+		leftCylRitem->Geo = mGeometries["shapeGeo"].get();
+		leftCylRitem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+		leftCylRitem->IndexCount = leftCylRitem->Geo->DrawArgs["cylinder"].IndexCount;
+		leftCylRitem->StartIndexLocation = leftCylRitem->Geo->DrawArgs["cylinder"].StartIndexLocation;
+		leftCylRitem->BaseVertexLocation = leftCylRitem->Geo->DrawArgs["cylinder"].BaseVertexLocation;
+
+		XMStoreFloat4x4(&rightCylRitem->World, leftCylWorld);
+		XMStoreFloat4x4(&rightCylRitem->TexTransform, brickTexTransform);
+		rightCylRitem->ObjCBIndex = objCBIndex++;
+		rightCylRitem->Mat = mMaterials["bricks0"].get();
+		rightCylRitem->Geo = mGeometries["shapeGeo"].get();
+		rightCylRitem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+		rightCylRitem->IndexCount = rightCylRitem->Geo->DrawArgs["cylinder"].IndexCount;
+		rightCylRitem->StartIndexLocation = rightCylRitem->Geo->DrawArgs["cylinder"].StartIndexLocation;
+		rightCylRitem->BaseVertexLocation = rightCylRitem->Geo->DrawArgs["cylinder"].BaseVertexLocation;
+
+		XMStoreFloat4x4(&leftSphereRitem->World, leftSphereWorld);
+		leftSphereRitem->TexTransform = MathHelper::Identity4x4();
+		leftSphereRitem->ObjCBIndex = objCBIndex++;
+		leftSphereRitem->Mat = mMaterials["mirror0"].get();
+		leftSphereRitem->Geo = mGeometries["shapeGeo"].get();
+		leftSphereRitem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+		leftSphereRitem->IndexCount = leftSphereRitem->Geo->DrawArgs["sphere"].IndexCount;
+		leftSphereRitem->StartIndexLocation = leftSphereRitem->Geo->DrawArgs["sphere"].StartIndexLocation;
+		leftSphereRitem->BaseVertexLocation = leftSphereRitem->Geo->DrawArgs["sphere"].BaseVertexLocation;
+
+		XMStoreFloat4x4(&rightSphereRitem->World, rightSphereWorld);
+		rightSphereRitem->TexTransform = MathHelper::Identity4x4();
+		rightSphereRitem->ObjCBIndex = objCBIndex++;
+		rightSphereRitem->Mat = mMaterials["mirror0"].get();
+		rightSphereRitem->Geo = mGeometries["shapeGeo"].get();
+		rightSphereRitem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+		rightSphereRitem->IndexCount = rightSphereRitem->Geo->DrawArgs["sphere"].IndexCount;
+		rightSphereRitem->StartIndexLocation = rightSphereRitem->Geo->DrawArgs["sphere"].StartIndexLocation;
+		rightSphereRitem->BaseVertexLocation = rightSphereRitem->Geo->DrawArgs["sphere"].BaseVertexLocation;
+
+		mRitemLayer[(int)RenderLayer::Opaque].push_back(leftCylRitem.get());
+		mRitemLayer[(int)RenderLayer::Opaque].push_back(rightCylRitem.get());
+		mRitemLayer[(int)RenderLayer::Opaque].push_back(leftSphereRitem.get());
+		mRitemLayer[(int)RenderLayer::Opaque].push_back(rightSphereRitem.get());
+
+		mAllRitems.push_back(std::move(leftCylRitem));
+		mAllRitems.push_back(std::move(rightCylRitem));
+		mAllRitems.push_back(std::move(leftSphereRitem));
+		mAllRitems.push_back(std::move(rightSphereRitem));
+	}
+
+    for(UINT i = 0; i < mSkinnedMats.size(); ++i)
+    {
+        std::string submeshName = "sm_" + std::to_string(i);	//对于蒙皮角色, 其有数个submesh. 每个我们都需要一个对应的渲染项
+
+        auto ritem = std::make_unique<RenderItem>();
+
+        // Reflect to change coordinate system from the RHS the data was exported out as.
+        XMMATRIX modelScale = XMMatrixScaling(0.05f, 0.05f, -0.05f);
+        XMMATRIX modelRot = XMMatrixRotationY(MathHelper::Pi);
+        XMMATRIX modelOffset = XMMatrixTranslation(0.0f, 0.0f, -5.0f);
+        XMStoreFloat4x4(&ritem->World, modelScale*modelRot*modelOffset);
+
+        ritem->TexTransform = MathHelper::Identity4x4();
+        ritem->ObjCBIndex = objCBIndex++;
+        ritem->Mat = mMaterials[mSkinnedMats[i].Name].get();
+        ritem->Geo = mGeometries[mSkinnedModelFilename].get();
+        ritem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+        ritem->IndexCount = ritem->Geo->DrawArgs[submeshName].IndexCount;
+        ritem->StartIndexLocation = ritem->Geo->DrawArgs[submeshName].StartIndexLocation;
+        ritem->BaseVertexLocation = ritem->Geo->DrawArgs[submeshName].BaseVertexLocation;
+
+        // All render items for this solider.m3d instance share
+        // the same skinned model instance.
+        ritem->SkinnedCBIndex = 0;		//蒙皮动画角色还有蒙皮常量缓冲区,与蒙皮模型实例
+        ritem->SkinnedModelInst = mSkinnedModelInst.get();
+
+        mRitemLayer[(int)RenderLayer::SkinnedOpaque].push_back(ritem.get());
+        mAllRitems.push_back(std::move(ritem));
+    }
 }
 
 void SkinnedMeshApp::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems)
 {
+	UINT objCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));	//获取每个物体常量和蒙皮常量缓冲区的大小
+	UINT skinnedCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(SkinnedConstants));
+
+	auto objectCB = mCurrFrameResource->ObjectCB->Resource();	//获取当前的物体常量缓冲区, 与蒙皮常量缓冲区
+	auto skinnedCB = mCurrFrameResource->SkinnedCB->Resource();
+
+	for (size_t i = 0; i < ritems.size(); ++i)
+	{
+		auto ri = ritems[i];
+
+		cmdList->IASetVertexBuffers(0, 1, &ri->Geo->VertexBufferView());	//设置当前渲染的物体的顶点缓冲区
+		cmdList->IASetIndexBuffer(&ri->Geo->IndexBufferView());	//设置当前渲染的物体的索引缓冲区
+		cmdList->IASetPrimitiveTopology(ri->PrimitiveType);
+
+		D3D12_GPU_VIRTUAL_ADDRESS objCBAddress = objectCB->GetGPUVirtualAddress() + ri->ObjCBIndex * objCBByteSize;	//获取当前渲染物体对应的物体缓冲区的位置
+		cmdList->SetGraphicsRootConstantBufferView(0, objCBAddress);	//设置根参数的0, 其绑定了物体的常量缓冲区
+
+		if (ri->SkinnedModelInst != nullptr)
+		{
+			D3D12_GPU_VIRTUAL_ADDRESS skinnedCBAddress = skinnedCB->GetGPUVirtualAddress() + ri->SkinnedCBIndex * skinnedCBByteSize;	//计算当前蒙皮模型对应的物体缓冲区的位置!!!
+			cmdList->SetGraphicsRootConstantBufferView(1, skinnedCBAddress);	//如果有蒙皮模型, 则将蒙皮模型绑定到根参数1
+		}
+		else 
+		{
+			cmdList->SetGraphicsRootConstantBufferView(1, 0);	//否则, 我们将空绑定到根参数1
+		}
+
+		cmdList->DrawIndexedInstanced(ri->IndexCount, 1, ri->StartIndexLocation, ri->BaseVertexLocation, 0);	//绘制调用, 索引数为indexCount, 起始索引为StartIndexLocation, 顶点偏移为BaseVertexLocation, 绘制1次
+	}
 }
 
 void SkinnedMeshApp::DrawSceneToShadowMap()
